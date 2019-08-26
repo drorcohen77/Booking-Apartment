6534bcf406c28007574207f3c5fc3fc62d422b61
drshualy <drorc77@gmail.com>
2019-08-26 21:53:08 +0300

    css

--- src/app/main/rental-detail/rental-detail-booking/rental-detail-booking.component.ts
+++ src/app/main/rental-detail/rental-detail-booking/rental-detail-booking.component.ts
@@ -1,16 +1,18 @@
 import { BookingService } from './../../../Services/booking.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input , ViewChild, ViewEncapsulation } from '@angular/core';
 import {Booking} from '../../../Services/booking.model';
 import { HelperService } from 'src/app/Services/helper.service';
 import { Rental } from 'src/app/Services/rental.model';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import * as moment from 'moment';
+import { DaterangePickerComponent } from 'ng2-daterangepicker';
 import { ToastrService } from 'ngx-toastr';
 
 
 
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-rental-detail-booking',
   templateUrl: './rental-detail-booking.component.html',
   styleUrls: ['./rental-detail-booking.component.scss']
@@ -18,6 +20,8 @@ import { ToastrService } from 'ngx-toastr';
 export class RentalDetailBookingComponent implements OnInit {
 
   @Input() rental: Rental;
+  @ViewChild(DaterangePickerComponent)
+    private picker: DaterangePickerComponent;
 
   newBooking: Booking;
   modalRef:any;
@@ -32,6 +36,7 @@ export class RentalDetailBookingComponent implements OnInit {
       locale: { format: Booking.BOOKING_FORMAT },
       alwaysShowCalendars: false,
       opens: 'left',
+      autoUpdateInput: false,
       isInvalidDate: this.checkForInvalidDates.bind(this)
   };
 
@@ -67,10 +72,15 @@ export class RentalDetailBookingComponent implements OnInit {
     this.bookedOutDates.push(...dateRange);
   }
 
+  private resetDatePicker() {
+    this.picker.datePicker.setStartDate(moment());
+    this.picker.datePicker.setEndDate(moment());
+    this.picker.datePicker.element.val('');
+  }
+
   openConfirmModal(content){
     this.errors = [];
     this.modalRef = this.modalService.open(content);
-    console.log(this.newBooking);
   }
 
   createBooking() {
@@ -81,6 +91,7 @@ export class RentalDetailBookingComponent implements OnInit {
         this.addNewBookedDates(bookingData);
         this.newBooking = new Booking();
         this.modalRef.close();
+        this.resetDatePicker();
         this.toastr.success('Booking has been succesfully created, check your booking detail in manage section', 'Success!');
       },
       (errorResponse: any) =>{
@@ -89,6 +100,7 @@ export class RentalDetailBookingComponent implements OnInit {
   }
 
     public selectedDate(value: any, datepicker?: any) {
+        this.options.autoUpdateInput = true;
         this.newBooking.startAt = this.helper.formatBookingDate(value.start);
         this.newBooking.endAt = this.helper.formatBookingDate(value.end);
         this.newBooking.days = -(value.start.diff(value.end, 'days'));
